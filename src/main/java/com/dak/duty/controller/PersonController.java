9c290cf324ec772a9c9f1c8653f3606fe0d61cb7
David Key <davidkey@gmail.com>
2015-05-24 15:19:49 -0400

    restricted a couple of methods to GET only

--- src/main/java/com/dak/duty/controller/PersonController.java
+++ src/main/java/com/dak/duty/controller/PersonController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.dak.duty.model.Person;
@@ -16,7 +17,7 @@ public class PersonController {
    @Autowired
    PersonRepository personRepos;
    
-   @RequestMapping("/{personId}")
+   @RequestMapping(value = "/{personId}", method = RequestMethod.GET)
    @ResponseBody Person getPersonById(@PathVariable Long personId){
       return personRepos.findOne(personId);
    }
