c95acb88bd3ea6d997c49e67f2d8c62bf81b6641
David Key <davidkey@gmail.com>
2015-05-30 16:53:15 -0400

    cleanup

--- src/main/java/com/dak/duty/controller/AdminController.java
+++ src/main/java/com/dak/duty/controller/AdminController.java
@@ -15,8 +15,6 @@ import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
