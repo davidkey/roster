fc4dc028f21dadc57192c77c01ad0546f5e9e34c
David Key <davidkey@gmail.com>
2015-06-03 23:09:55 -0400

    clarify comments for DutyService

--- src/main/java/com/dak/duty/service/DutyService.java
+++ src/main/java/com/dak/duty/service/DutyService.java
@@ -21,14 +21,18 @@ public class DutyService {
    public Duty saveOrUpdateDuty(Duty duty){
 
       /**
-       * 1) Decrement all sort orders [>] old sort order (if this is an update, not a new item)
-       * 2) Increment all sort orders [>=] new / updated duty sort order IF this isn't an inactive duty
-       * 3) Persist duty w/ new sort order
+       * If this is a soft delete:
+       *  1) Update name accordingly, decrement sort order [>=] current duty.sortOrder
+       * 
+       * Otherwise (update or a new duty)
+       *  1) Decrement all sort orders [>] old sort order (if this is an update, not a new item)
+       *  2) Increment all sort orders [>=] new / updated duty sort order IF this isn't an inactive duty
+       *  3) Persist duty w/ new sort order
        */
       if(duty.getId() > 0){ // if this is an update, not a new entity
          final Duty dutyBeforeChanges = dutyRepos.findOne(duty.getId());
 
-         if(!duty.getActive() && dutyBeforeChanges.getActive()){ // if we're deactivating this duty ...
+         if(!duty.getActive() && dutyBeforeChanges.getActive()){ // if we're deactivating / soft deleting this duty ...
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
             
             duty.setName(duty.getName() + " (deleted @ " + sdf.format(new Date()) + ")"); // change name to show soft delete and to prevent key errors if another with same name added later
