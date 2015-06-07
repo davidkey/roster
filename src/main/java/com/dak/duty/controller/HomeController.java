d6fc3508cad6d2bc9681e86f9aa449e38cacca84
David Key <davidkey@gmail.com>
2015-06-07 18:34:31 -0400

    removed extra requestparam

--- src/main/java/com/dak/duty/controller/HomeController.java
+++ src/main/java/com/dak/duty/controller/HomeController.java
@@ -46,13 +46,12 @@ public class HomeController {
    @RequestMapping(value = "/login", method = RequestMethod.GET)
    public String getLogin(
          @RequestParam(value = "error", required = false) String error, 
-         @RequestParam(value = "logout", required = false) String logout,
          Model model, HttpServletRequest request){
 
       if (error != null) {
          model.addAttribute("error", "Invalid username and password!");
       }
-      
+
       return "login";
    }
 }
