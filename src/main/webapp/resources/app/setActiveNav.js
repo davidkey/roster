65487dae55236d369fddfb72f7116d2feaf13f64
David Key <davidkey@gmail.com>
2015-06-13 18:25:35 -0400

    fixing setActiveNav for deployments to ROOT context

--- src/main/webapp/resources/app/setActiveNav.js
+++ src/main/webapp/resources/app/setActiveNav.js
@@ -6,9 +6,16 @@
     	
     	function setCurrentActivePage(){
     		var pieces = window.location.pathname.split('/');
-    		var navBarId = pieces[2] === 'admin' ? 'navsidebar' : 'navbar';
-    		var activePage = 'nav' + capitalize(pieces[3] ? pieces[3] : pieces[2]);
-
+    		var navBarId = '';
+    		var activePage = '';
+    		
+    		if(WEB_ROOT().length === 0){ /* for when app is deployed to ROOT context */
+    			narBarId = pieces[1] === 'admin' ? 'navsidebar' : 'navbar';
+    			activePage = 'nav' + capitalize(pieces[2] ? pieces[2] : pieces[1]);
+    		} else {
+    			narBarId = pieces[2] === 'admin' ? 'navsidebar' : 'navbar';
+    			activePage = 'nav' + capitalize(pieces[3] ? pieces[3] : pieces[2]);
+    		}
     		
     		$('#' + navBarId + ' ul li.active').removeClass('active');
     		$('#' + navBarId + ' ul #' + activePage).addClass('active');
