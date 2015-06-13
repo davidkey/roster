aa39748d50d3fe7feaee88a8b5d740bd76268176
David Key <davidkey@gmail.com>
2015-06-13 18:28:48 -0400

    fixing typo (i hate js!)

--- src/main/webapp/resources/app/setActiveNav.js
+++ src/main/webapp/resources/app/setActiveNav.js
@@ -10,10 +10,10 @@
     		var activePage = '';
     		
     		if(WEB_ROOT().length === 0){ /* for when app is deployed to ROOT context */
-    			narBarId = pieces[1] === 'admin' ? 'navsidebar' : 'navbar';
+    			navBarId = pieces[1] === 'admin' ? 'navsidebar' : 'navbar';
     			activePage = 'nav' + capitalize(pieces[2] ? pieces[2] : pieces[1]);
     		} else {
-    			narBarId = pieces[2] === 'admin' ? 'navsidebar' : 'navbar';
+    			navBarId = pieces[2] === 'admin' ? 'navsidebar' : 'navbar';
     			activePage = 'nav' + capitalize(pieces[3] ? pieces[3] : pieces[2]);
     		}
     		
