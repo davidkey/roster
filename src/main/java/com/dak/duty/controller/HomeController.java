fe308da5d831e30632f48a996e400d5b252941de
David Key <davidkey@gmail.com>
2015-06-21 23:41:38 -0400

    removed throwAnError() method :)

--- src/main/java/com/dak/duty/controller/HomeController.java
+++ src/main/java/com/dak/duty/controller/HomeController.java
@@ -44,10 +44,4 @@ public class HomeController {
 
       return "login";
    }
-   
-   @RequestMapping(value = "/error", method = RequestMethod.GET)
-   public String throwAnError(){
-      
-      throw new RuntimeException("yup, this is an error");
-   }
 }
